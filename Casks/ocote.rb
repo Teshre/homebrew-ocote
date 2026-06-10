78d56833e079ffd287055b9147bb2c3c0bba9d89
Teshre <ing.e.perry.rangel@gmail.com>
2026-06-09 22:53:33 -0600

    Update ocote cask to v0.5.9

--- Casks/ocote.rb
+++ Casks/ocote.rb
@@ -1,6 +1,6 @@
 cask "ocote" do
-  version "0.6.0"
-  sha256 "cd662d744351e44aeef2429c8b69e0e7158bbc75014c52462fab83637c289c0f"
+  version "0.5.9"
+  sha256 "a28b8c3b0c65d57d6d9ecdb22767dcc8b6e1c11557a683ddfecc32f8f5dba2d0"
 
   url "https://github.com/Teshre/Ocote/releases/download/v#{version}/Ocote_#{version}_aarch64.dmg",
       verified: "github.com/Teshre/Ocote/"
